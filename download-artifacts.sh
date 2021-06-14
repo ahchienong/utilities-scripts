37dc3acefcc6ad6a3ff03d74d29140b4d774f6e3
ahchienong <ahchienong@gmail.com>
2021-06-14 20:32:39 +0800

    Create download-artifacts.sh

---
+++ download-artifacts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Configure YOUR ACCESS TOKEN
+# Configure YOUR PROJECT ARTIFACTS URL
+YOUR_ACCESS_TOKEN={YOUR_ACCESS_TOKEN}
+YOUR_PROJECT_ARTIFACTS_URL={YOUR_PROJECT_ARTIFACTS_URL}
+
+## Download the artifact
+curl --output artifacts.zip --header "PRIVATE-TOKEN: $YOUR_ACCESS_TOKEN" $YOUR_PROJECT_ARTIFACTS_URL
+
+## Unzip the file
+unzip artifacts.zip 
