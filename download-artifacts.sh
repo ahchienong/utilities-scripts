bd2e2339a2ce02db15324d0fa1d8510debfde58a
ahchienong <ahchienong@gmail.com>
2021-06-14 21:27:49 +0800

    Update download-artifacts.sh

--- download-artifacts.sh
+++ download-artifacts.sh
@@ -4,6 +4,9 @@
 # 0. Configuration
 # Configure YOUR ACCESS TOKEN
 # Configure YOUR PROJECT ARTIFACTS URL
+# 
+# Gitlab Personal Token: https://gitlab.com/-/profile/personal_access_tokens
+# Gitlab API ref: https://docs.gitlab.com/ee/api/job_artifacts.html#download-the-artifacts-archive
 ########################################
 YOUR_ACCESS_TOKEN={YOUR_ACCESS_TOKEN}
 YOUR_PROJECT_ARTIFACTS_URL={YOUR_PROJECT_ARTIFACTS_URL}
