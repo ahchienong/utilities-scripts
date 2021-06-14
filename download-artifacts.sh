e19a232bc57ca0a14d2289cc9c58baffd3831327
ahchienong <ahchienong@gmail.com>
2021-06-14 21:19:51 +0800

    Update download-artifacts.sh

--- download-artifacts.sh
+++ download-artifacts.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+########################################
+# 0. Configuration
 # Configure YOUR ACCESS TOKEN
 # Configure YOUR PROJECT ARTIFACTS URL
+########################################
 YOUR_ACCESS_TOKEN={YOUR_ACCESS_TOKEN}
 YOUR_PROJECT_ARTIFACTS_URL={YOUR_PROJECT_ARTIFACTS_URL}
 
-## Download the artifact
-#curl --output artifacts.zip --header "PRIVATE-TOKEN: $YOUR_ACCESS_TOKEN" $YOUR_PROJECT_ARTIFACTS_URL
+########################################
+# 1. Download the artifact
+########################################
 wget -o artifacts.zip --header="PRIVATE-TOKEN: $YOUR_ACCESS_TOKEN" $YOUR_PROJECT_ARTIFACTS_URL
 
-## Unzip the file
+########################################
+# 2. remove artifacts folder (if exists)
+########################################
+rm -rf artifacts
+
+########################################
+# 3. Unzip the zip file
+########################################
 unzip artifacts.zip -d artifacts
