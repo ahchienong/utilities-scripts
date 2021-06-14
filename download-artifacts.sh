adbea4e7d8736b0f1163dc35e57d9469d622f722
ahchienong <ahchienong@gmail.com>
2021-06-14 21:10:22 +0800

    update file to save and unzip directory

--- download-artifacts.sh
+++ download-artifacts.sh
@@ -7,7 +7,7 @@ YOUR_PROJECT_ARTIFACTS_URL={YOUR_PROJECT_ARTIFACTS_URL}
 
 ## Download the artifact
 #curl --output artifacts.zip --header "PRIVATE-TOKEN: $YOUR_ACCESS_TOKEN" $YOUR_PROJECT_ARTIFACTS_URL
-wget --header="PRIVATE-TOKEN: $YOUR_ACCESS_TOKEN" $YOUR_PROJECT_ARTIFACTS_URL
+wget -o artifacts.zip --header="PRIVATE-TOKEN: $YOUR_ACCESS_TOKEN" $YOUR_PROJECT_ARTIFACTS_URL
 
 ## Unzip the file
-unzip artifacts.zip 
+unzip artifacts.zip -d artifacts
